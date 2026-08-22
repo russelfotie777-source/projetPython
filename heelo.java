701051ae42e93d04a7c786f900a14e0e0a2ea1c2
Ton Nom <tonemail@example.com>
2026-08-22 06:10:33 +0200

    mes conditions

--- heelo.java
+++ heelo.java
@@ -1,30 +1,19 @@
+
 import java.util.Scanner;
-
-
 public class heelo {
     public static void main(String[] args)
     {
-        //Projet Cobvertisseurs
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Entrez Votre nom : ");
-        String nom = sc.nextLine();
-        System.out.print("Entrez le poid en Km : ");
-        double poids = sc.nextDouble();
-        System.out.println("Entrer le KG : ");
-        double kg = sc.nextDouble();
+    Scanner scanner = new Scanner(System.in);
+    int nombre = scanner.nextInt();
 
-        double conversion = poids * 1000;
+    //comparaison
+     if (nombre > 0) {
+        System.out.println("Le nombre est positif.");
 
-        System.out.println("Entrer la température en Celsius : ");
-        double celsius = sc.nextDouble();
-        double fahrenheit = (celsius * 9/5) + 32;
-
-        System.out.println("Bonjour " + nom + " le poid en gramme est : " + conversion + "g");
-        System.out.println("Le poid en Kg est : " + kg + "Kg");
-        System.out.println("La température en Fahrenheit est : " + fahrenheit + "°F");
-
-
-        
-        
+    }else if (nombre < 0) {
+        System.out.println("Le nombre est négatif.");
+    } else {
+        System.out.println("Le nombre est zéro.");
     }
+}
 }
